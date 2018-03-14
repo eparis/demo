9f635db6975340b1f893f0f5a19576f137269746
Eric Paris <eparis@redhat.com>
2018-03-14 11:46:24 -0400

    made a code change

--- pythonapp/myserver.py
+++ pythonapp/myserver.py
@@ -20,7 +20,7 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write(bytes("<html><body><h1>hi!</h1></body></html>", 'UTF-8'))
+        self.wfile.write(bytes("<html><body><h1>hi you guys!</h1></body></html>", 'UTF-8'))
 
     def do_HEAD(self):
         self._set_headers()
